c39be03d12905788de7dcdc7d4180fbcba1cc6d4
Izabelle Datayan <i_datayan@yahoo.com>
2022-09-06 11:20:10 -0700

    Fixed checkstyle errors

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,11 @@
 package org.comp2522;
 
+/**
+ * This program prints a character from "That Time I Reincarnated as a Slime."
+ */
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Rimuru Tempest");
-    }
+  public static void main(String[] args) {
+    System.out.println("Rimuru Tempest");
+  }
 }
